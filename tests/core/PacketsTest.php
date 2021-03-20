3f822c3c4e2da2e189a8dbd77b546c2c255d130b
Michael Chekin <mchekin@gmail.com>
2021-03-20 09:13:05 +0100

    Unit Tests: adding unit tests for Packets::addUInt8() and Packets::addInt8()

--- tests/core/PacketsTest.php
+++ tests/core/PacketsTest.php
@@ -103,6 +103,28 @@ class PacketsTest extends TestCase
         self::assertSame("00" . $hexPrefix . $hexString, $sut->getPacketStr());
     }
 
+    public function test_add_u_int_8()
+    {
+        $integer = 66;
+
+        $sut = new Packets();
+
+        $sut->addUInt8($integer);
+
+        self::assertSame("00" . dechex($integer), $sut->getPacketStr());
+    }
+
+    public function test_add_int_8()
+    {
+        $integer = 66;
+
+        $sut = new Packets();
+
+        $sut->addInt8($integer);
+
+        self::assertSame("00" . dechex($integer), $sut->getPacketStr());
+    }
+
     public function test_int8_packs_integer_into_hex_string()
     {
         self::assertSame('20', Packets::int8(32));
